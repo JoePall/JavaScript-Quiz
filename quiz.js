7a829f15161cc9d7aaedf9aeb8c26eff252895cb
Josiah Powell <josiahpowell@outlook.com>
2020-06-30 00:55:50 -0500

    updating index.html and quiz.js

---
+++ quiz.js
@@ -0,0 +1,10 @@
+// create Game class
+    // create function stop
+    // create function start
+    // create function updateHighScores
+    // create function refreshDisplay
+    // create function askQuestion
+    
+    // create button start
+    // create button Yes
+    // create button No    
