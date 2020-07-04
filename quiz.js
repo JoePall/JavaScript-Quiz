4695d315b1efe43125325338fbb603897f496c2e
Josiah Powell <josiahpowell@outlook.com>
2020-07-04 13:55:15 -0500

    Updated everything using Jquery! :)

--- quiz.js
+++ quiz.js
@@ -1,92 +1,132 @@
-var responseSection = document.getElementById("response-section");
-var buttonNo = document.getElementById("no-button");
-var buttonYes = document.getElementById("yes-button");
-var buttonStart = document.getElementById("start-game-button");
-var statusPrimary = document.getElementById("status-primary");
-var statusSecondary = document.getElementById("status-secondary");
-
-
-buttonStart.addEventListener("click", function(event) {
-    event.preventDefault();
-    game.startQuiz();
-});    
-
-buttonNo.addEventListener("click", function(event) {
-    event.preventDefault();
-    game.onAnswer(false);
-});    
-
-buttonYes.addEventListener("click", function(event) {
-    event.preventDefault();
-    game.onAnswer(true);
-});
-
-
+// JavaScript Quiz Game
 var game = {
-    timerInterval: "",
     highScores: [],
-    allotedTime: 120,
-    questions: [["Is JavaScript case sensitive?", true], ['Is "===" type sensitive?', false], ["#3", false]], // JavaScript fundamentals questions
-    currentQuestionIndex: 0,
     score: 0,
-    onAnswer: function(response) {
-        if (this.currentQuestionIndex == (this.questions.length - 1)) {
-            this.stop();
-            return;
-        }
-        else if (response == this.questions[this.currentQuestionIndex][1]) {
-            this.score++;
-        }
-        this.currentQuestionIndex++;
-        this.askQuestion();
-    },
-    setup: function() {
-        responseSection.style.visibility = "hidden";
-    },
-    stop: function() {
-        clearInterval(timerInterval);
-        statusPrimary.textContent = "Game Over!";
-        statusSecondary.textContent = "Score: " + this.score + "! New Game?";
-        buttonStart.style.visibility = "visible";
-        responseSection.style.visibility = "hidden";
-    },
-    askQuestion: function() {
-        statusPrimary.textContent = this.questions[this.currentQuestionIndex][0];
+    questionIndex: 0,
+    timerInterval: "",
+    allotedTime: 120,
+    elapsedTime: 0,
 
+    // JavaScript fundamentals questions
+    questions: [["Is JavaScript case sensitive?", true],
+    ['Is <code>==</code> type sensitive?', false],
+    ['Is <code>var array = {};</code> a valid implementation of an array?', false],
+    ['Is <code>alert("Are you sure about this?");</code> valid in JavaScript?', true],
+    ['Is <code>for (let i = 0; i < array.length; i++)</code> a valid for loop in JavaScript?', true],
+    ['Is <code>(7 => 5)</code> valid in JavaScript?', false]],
+
+    // Initialize default values
+    init: () => {
+        // Hide quiz buttons and display start button
+        $("#start-button").fadeIn(1);
+        $("#yes-button").fadeOut(1);
+        $("#no-button").fadeOut(1);
+
+        // Sets the allotted time based on the amount of questions
+        game.allotedTime = game.questions.length * 15;
     },
-    startQuiz: function() {
-        this.currentQuestionIndex = 0;
-        this.askQuestion();
-        
-        buttonStart.style.visibility = 'hidden';
-        responseSection.style.visibility = 'visible';
-        
-        this.startTimer();
+
+    // Displays a question to the user
+    askQuestion: () => $("#status").html(game.questions[game.questionIndex][0]),
+
+    // Handles the users response to the question
+    onResponse: (response) => {
+        // If the users answer matched the games answer increment the score by 1 and display it.
+        if (response == game.questions[game.questionIndex][1]) {
+            $("#score").html("Score: " + ++game.score);
+        }
+
+        // Stops the game if there are no more questions, otherwise ask the next question.
+        if (++game.questionIndex >= game.questions.length) game.stopQuiz();
+        else game.askQuestion();
     },
-    startTimer: function() {
-        var timeLeft = this.allotedTime;
-        timerInterval = setInterval(function () {
-            statusSecondary.textContent = timeLeft + " seconds left";
+
+    // Starts the Quiz 
+    startQuiz: () => {
+        // Set Default Values for variables
+        game.questionIndex = 0;
+        game.score = 0;
+
+        // Display quiz buttons and hide start button
+        $("#start-button").fadeOut(10);
+        $("#yes-button").fadeIn(10);
+        $("#no-button").fadeIn(10);
+        $("#player-name").fadeOut(10);
+
+        //Begin Game
+        game.startTimer();
+        game.askQuestion();
+    },
+
+    // Stops the Quiz
+    stopQuiz: () => {
+        game.stopTimer();
+        highScores = game.storeHighScores();
+
+        var result = "Game Over!<br><br>High Scores<br>";
+        $.each(highScores, (i, score) => {
+            var text = score[0] + ": " + score[1];
+            if (text == ($("#player-name").val() + ": " + score)) result += "<b>" + text + "</b><br>";
+            else result += text + "<br>";
+        });
+        $("#status").html(result);
+
+        $("#start-button").fadeIn(10);
+        $("#yes-button").fadeOut(10);
+        $("#no-button").fadeOut(10);
+        $("#player-name").fadeIn(10);
+    },
+
+    // Starts the timer
+    startTimer: () => {
+        var timeLeft = game.allotedTime;
+        game.timerInterval = setInterval(() => {
+            $("#timer").html(timeLeft + " seconds left");
             timeLeft--;
             if (timeLeft < 0) {
-                stop();
-                clearInterval(timerInterval);
+                clearInterval(game.timerInterval);
+                game.stopQuiz();
             }
         }, 1000);
     },
+
+    // Stops the timer
+    stopTimer: () => {
+        clearInterval(game.timerInterval);
+        game.elapsedTime = 0;
+    },
+
+    // Updates the high scores and stores to local storage
+    storeHighScores: () => {
+        // Get the current high scores
+        var highScores = JSON.parse(localStorage.getItem("highScores"));
+        var value = [$("#player-name").val(), game.score];
+
+        if (highScores == undefined) highScores = [value];
+        else {
+            // Add the value
+            highScores.push(value);
+
+            // Sort all and get top 5
+            highScores = highScores.sort((a, b) => b[1] - a[1]);
+
+            console.log(highScores);
+
+        }
+
+        if (highScores.length > 5) {
+            highScores.splice(5);
+        }
+
+        // Save the top 5
+        localStorage.setItem("highScores", JSON.stringify(highScores));
+
+        return highScores;
+    },
 }
 
-game.setup();
+$("#start-button").click(() => game.startQuiz());
+$("#no-button").click(() => game.onResponse(false));
+$("#yes-button").click(() => game.onResponse(true));
 
-
-// // GIVEN I am taking a code quiz
-// // WHEN I click the start button
-// // THEN a timer starts and I am presented with a question
-// // WHEN I answer a question
-// // THEN I am presented with another question
-// // WHEN I answer a question incorrectly
-// // THEN time is subtracted from the clock
-// // WHEN all questions are answered or the timer reaches 0
-// // THEN the game is over
-// // WHEN the game is over
-// // THEN I can save my initials and score
+game.init();
